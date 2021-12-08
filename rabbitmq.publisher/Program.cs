056340925a0f0c18119ff92fa391cc754022f5aa
Sercan Sever <sercan.sever16@gmail.com>
2021-12-08 15:31:16 +0300

    initial

---
+++ rabbitmq.publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rwxubjos:j1HoQTDNTJ4j-Qv5UKzqfeX1uHdwIMKt@hornet.rmq.cloudamqp.com/rwxubjos");
+
+using (var connection = factory.CreateConnection()) //connection
+{
+   var channel = connection.CreateModel(); //channel
+   channel.QueueDeclare(
+                        queue: "hello-queue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false);
+   Enumerable.Range(1,50).ToList().ForEach(x =>
+   {
+
+      string message = $"Message {x} !";  //byte dizisi olarak gider.
+      var messageBody = Encoding.UTF8.GetBytes(message);
+      channel.BasicPublish(
+                           exchange: string.Empty,
+                           routingKey: "hello-queue",
+                           basicProperties: null,
+                           body: messageBody);
+
+      Console.WriteLine($"{message} - Mesajınız gönderildi. Çıkmak için bir tuşa basınız");
+   });
+
+   Console.ReadLine();
+}
---
+++ rabbitmq.subscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rwxubjos:j1HoQTDNTJ4j-Qv5UKzqfeX1uHdwIMKt@hornet.rmq.cloudamqp.com/rwxubjos");
+
+using (var connection = factory.CreateConnection()) //connection
+{
+   var channel = connection.CreateModel(); //channel
+   // channel.QueueDeclare("hello-queue", false, false, false);
+
+   channel.BasicQos(0, 1, false);
+   var consumer = new EventingBasicConsumer(channel);
+   channel.BasicConsume(
+                        queue: "hello-queue",
+                        autoAck: false,
+                        consumer: consumer);
+
+
+
+   consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+   {
+      var body = e.Body.ToArray();
+      var message = Encoding.UTF8.GetString(body);
+      Thread.Sleep(1500);
+      Console.WriteLine("Incoming Message : " + message);
+
+      channel.BasicAck(e.DeliveryTag, false);
+   };
+   Console.ReadLine();
+}
